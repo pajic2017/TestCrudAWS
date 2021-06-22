bbb6de6daf064d3368d6677fd933cd376a2821fa
pajic2017 <pajic.it.2017@gmail.com>
2021-06-22 11:36:16 +0200

    The thrid commit

--- src/main/java/com/may/contact/model/User.java
+++ src/main/java/com/may/contact/model/User.java
@@ -59,4 +59,9 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+	@Override
+	public String toString() {
+		return "User [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
 }
